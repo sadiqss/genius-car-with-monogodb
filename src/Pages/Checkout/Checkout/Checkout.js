6441c5d744a720f598e9fbef65e2afbe72697e40
sadiqss <sadiq.sau48@gmail.com>
2022-04-30 04:11:22 +0100

    Data collection from form

--- src/Pages/Checkout/Checkout/Checkout.js
+++ src/Pages/Checkout/Checkout/Checkout.js
@@ -1,11 +1,14 @@
 import React, { useState } from 'react';
 import { useParams } from 'react-router-dom';
 import useServiceDetail from '../../../hooks/useServiceDetail';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import auth from '../../../firebase.init';
 
 const Checkout = () => {
     const { serviceId } = useParams();
     const [service] = useServiceDetail(serviceId);
-    const [user, setUser] = useState({
+    const [user] = useAuthState(auth);
+    /* const [user, setUser] = useState({
         name: 'Hudson',
         email: 'hudson@gmail.com',
         address: 'London',
@@ -17,20 +20,31 @@ const Checkout = () => {
         const newAddress = event.target.value;
         const newUser = { address: newAddress, ...rest };
         setUser(newUser);
+    } */
+    const handlePlaceOrder = event => {
+        event.preventDefault();
+        const order = {
+            email: user.email,
+            service: service.name,
+            serviceId: serviceId,
+            address: event.target.address.value,
+            phone: event.target.phone.value
+
+        }
     }
     return (
         <div className='w-50 mx-auto'>
             <h2>Please Order: {service.name}</h2>
-            <form>
-                <input className='w-100 mb-2' type="text" name="name" value={user.name} placeholder='name' required id="" />
+            <form onSubmit={handlePlaceOrder}>
+                <input className='w-100 mb-2' type="text" name="name" value={user.displayName} placeholder='name' required readOnly disabled id="" />
                 <br />
-                <input className='w-100 mb-2' type="email" name="email" value={user.email} placeholder='email' required id="" />
+                <input className='w-100 mb-2' type="email" name="email" value={user.email} placeholder='email' required readOnly disabled id="" />
                 <br />
                 <input className='w-100 mb-2' type="text" name="service" value={service.name} placeholder='service' required id="" />
                 <br />
-                <input className='w-100 mb-2' onChange={handleAddressChange} type="text" name="address" value={user.address} placeholder='address' required id="" />
+                <input className='w-100 mb-2' type="text" name="address" placeholder='address' autoComplete='off' required id="" />
                 <br />
-                <input className='w-100 mb-2' type="text" name="phone" value={user.phone} placeholder='phone' required id="" />
+                <input className='w-100 mb-2' type="text" name="phone" placeholder='phone' required id="" />
                 <br />
                 <input className='btn btn-primary' type="submit" value="Place Order" />
             </form>
